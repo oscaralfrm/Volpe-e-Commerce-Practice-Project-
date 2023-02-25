60dfc1091ce4e8a38f3afa10112efcb92e5b0c15
oscaralfrm <oscaralfrm1996@gmail.com>
2023-02-25 15:22:13 -0300

    Agregando arquitectura Django, primeros commits y configuraciones del Back-End de la página.

--- volpe/pages/scripts/index.js
+++ volpe/pages/scripts/index.js
@@ -1,7 +1,11 @@
 const hamburgerMenu = document.getElementById('bar');
 const navBar = document.getElementById('navbar');
 const closeButton = document.getElementById('close');
+const myAccount = document.getElementById('my-account');
+const accountOptions = document.getElementById('account-options');
 
+
+/* Function when handling responsive menus  // Navbar Hamburger Menu */
 if (hamburgerMenu) {
     hamburgerMenu.addEventListener('click', () => {
         navBar.classList.add('active');
@@ -13,5 +17,19 @@ if (closeButton) {
     closeButton.addEventListener('click', () => {
         navBar.classList.remove('active');
         hamburgerMenu.classList.remove('inactive');
+    })
+}
+
+/* Toggle My Account Options */
+
+if (myAccount) {
+    myAccount.addEventListener('mouseover', () => {
+        accountOptions.classList.add('active');
+    })
+}
+
+if (accountOptions) {
+    accountOptions.addEventListener('mouseleave', () => {
+        accountOptions.classList.remove('active');
     })
 }
