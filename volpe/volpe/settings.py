ab3830c97a8e68c27d4b5f6b6e78d8aecd96b9d4
oscaralfrm <oscaralfrm1996@gmail.com>
2023-02-25 19:58:38 -0300

    Actualización e implementación estática del CSS y JS

--- volpe/volpe/settings.py
+++ volpe/volpe/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import Config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'volpe/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- volpe/volpe/views.py
+++ volpe/volpe/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'pages\index.html', {})
+    return render(request, 'index.html', {})
+
+def dinamico(request, name):
+    context = { 'name': name }
+    return render(request, 'dinamico.html', context)
